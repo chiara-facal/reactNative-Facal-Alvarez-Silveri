10cd120c3382be112827c3340dddd9e8a031dfa4
Cfacal <Cfacal@udesa.edu.ar>
2023-11-08 16:47:21 -0300

    Cambios en el buscador

--- proyecto-final/App.js
+++ proyecto-final/App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/screens/Login/Login';
 import Register from './src/screens/Register/Register';
+import Profile from './src/screens/Profile/Profile';
+import Search from './src/screens/Search/Search';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      <Search />
     </View>
   );
 }
