55bd2b61b0080f49a0a8fc14b4b56f6365bd8f3e
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-11-09 16:17:20 -0300

    nav

--- proyecto-final/App.js
+++ proyecto-final/App.js
@@ -4,20 +4,31 @@ import Login from './src/screens/Login/Login';
 import Register from './src/screens/Register/Register';
 import Profile from './src/screens/Profile/Profile';
 import Search from './src/screens/Search/Search';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Search />
-    </View>
-  );
+    
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen name='Registro' component={Register} options={ { headerShown: false } }/>
+        <Stack.Screen name='Login' component={Login} options={ { headerShown: false } }/>
+        <Stack.Screen name='Home' component={Profile} options={ { headerShown: true} }/>
+        <Stack.Screen name='Home' component={Search} options={ { headerShown: true } }/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+},
 });
